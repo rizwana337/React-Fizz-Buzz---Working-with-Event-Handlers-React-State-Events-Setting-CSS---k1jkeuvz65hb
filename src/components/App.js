3bdf57dc32f9a38ecb9d574eb66ce89c1c6f7dc2
rizwana337 <120568097+rizwana337@users.noreply.github.com>
2023-04-01 14:54:06 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react';
 import '../styles/App.css';
-const App = () => {
 
-  
+const App = () => {
+  const [count, setCount] = useState(1);
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+
+  const handleDecrement = () => {
+    setCount(count - 1);
+  };
+
+  let counterClass = "normal";
+
+  if (count % 3 === 0 && count % 5 === 0) {
+    counterClass = "fizzbuzz";
+  } else if (count % 3 === 0) {
+    counterClass = "fizz";
+  } else if (count % 5 === 0) {
+    counterClass = "buzz";
+  }
+
   return (
     <div id="main">
-
+      <div id="counter" className={counterClass}>
+        {count}
+      </div>
+      <button id="increment" onClick={handleIncrement}>
+        Increment
+      </button>
+      <button id="decrement" onClick={handleDecrement}>
+        Decrement
+      </button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
